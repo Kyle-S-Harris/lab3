61c198ed12a51addd5f133e61c74f9f41111e833
Kyle-S-Harris <kharris@ucsd.edu>
2020-01-23 11:21:02 -0800

    Added project click function

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,18 +1,40 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function() 
+{
 	initializePage();
+    projectClick();
 })
 
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$("#testjs").click(function(e) {
+function initializePage() 
+{
+	$("#testjs").click(function(e) 
+    {
 		$('.jumbotron h1').text("Javascript is connected");
+        $("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
+
+function projectClick(e)
+{
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) 
+    {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } 
+    else 
+    {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
+
